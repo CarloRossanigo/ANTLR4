c1d5dd3f1eef1698f9f5520df69e296caf249e2a
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-25 11:10:14 +0200

    Update comments to English in Program.java

--- Antlr4_SetTheory/src/expression/Program.java
+++ Antlr4_SetTheory/src/expression/Program.java
@@ -4,12 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Program {
-	public List<Expression> expressions;// Lista delle espressioni nel programma
-	// Costruttore che inizializza la lista delle espressioni
+	public List<Expression> expressions;// Expression list in the program 
+	// Constructor that initalize the expression list 
 	public Program() {
 		this.expressions=new ArrayList<>();
 	}
-	// Aggiunge un'espressione alla lista
+	//Adding an expression to the list 
 	public void addExpression(Expression e) {
 		expressions.add(e);
 	}
