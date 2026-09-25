6b03cde198b07b31a2023e850972f4812faae236
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-25 11:14:32 +0200

    Update comments in ValueBool.java to English

--- Antlr4_SetTheory/src/expression/ValueBool.java
+++ Antlr4_SetTheory/src/expression/ValueBool.java
@@ -3,14 +3,14 @@ package expression;
 import java.util.List;
 
 public class ValueBool extends Value {
-    private boolean value;	// Valore booleano associato
- // Costruttore che inizializza il valore booleano
+    private boolean value;	// Boolean value linked 
+ // Constructor to intialize the boolean value 
     public ValueBool(boolean v) {
         this.value = v;
     }
 
     @Override
-    // Restituisce il valore
+    // Return the value
     public List<Object> getValue() {
         return List.of(value);
     }
