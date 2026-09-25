6a326d553653ed57f409c4c3c9637bfc3f98eb8e
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-25 11:02:27 +0200

    Translate comments to English in AntlrToProgram.java

--- Antlr4_SetTheory/src/expression/AntlrToProgram.java
+++ Antlr4_SetTheory/src/expression/AntlrToProgram.java
@@ -7,19 +7,19 @@ import ANTLR4.TeoriaParser.ProgramContext;
 
 public class AntlrToProgram extends TeoriaBaseVisitor<Program>{
 
-	public List<String> semanticErrors; // lista degli errori semantici 
+	public List<String> semanticErrors; // Semantic error list  
 	@Override
 	public Program visitProgram(ProgramContext ctx) {
 		Program prog=new Program();
 		
 		semanticErrors= new ArrayList<>();
 		
-		// aiuto visitor per trasformare ogni sottoalbero in oggetto Espression
+		// Helping visitor to transform each undertree in an expression object 
 		
 		AntlrToExpression esprVisitor= new AntlrToExpression(semanticErrors);
 		for(int i=0;i<ctx.getChildCount();i++) {
 			if(i==ctx.getChildCount()-1) {
-				// l'ultimo figlio è EOF quindi non lo visito
+				//Last childis EOF so i don't visit it 
 			}
 			else {
 				prog.addExpression(esprVisitor.visit(ctx.getChild(i)));
