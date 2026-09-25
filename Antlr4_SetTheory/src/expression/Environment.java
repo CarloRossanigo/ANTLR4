b7513fd8b4f9fd2c3d02dc1573cbc60383b3812c
Carlo Rossanigo <140655402+CarloRossanigo@users.noreply.github.com>
2026-09-25 11:06:49 +0200

    Update comments in Environment.java to English

--- Antlr4_SetTheory/src/expression/Environment.java
+++ Antlr4_SetTheory/src/expression/Environment.java
@@ -3,7 +3,7 @@ package expression;
 import java.util.HashMap;
 
 public class Environment {
-    private HashMap<String, Value> context; // mappa degli insiemi e dei loro valori 
+    private HashMap<String, Value> context; // Sets and their values map  
 
     public Environment() {
         this.context = new HashMap<>();
@@ -14,18 +14,18 @@ public class Environment {
     }
 
     public void addSet(String name, Value value) {
-        context.put(name, value); // aggiunge un insieme al contesto
+        context.put(name, value); // Adding a set to the context 
     }
 
     public Value getVariable(Set name) {
         if (context.containsKey(name.toString())) {
-            return context.get(name.toString()); // restituisce il nome dell'insieme 
+            return context.get(name.toString()); // return the sets name  
         } else {
             throw new RuntimeException("Insieme non trovato: " + name);
         }
     }
 
     public Value evaluate(Expression expr) {
-        return expr.evaluate(this); // valuta l'espressione nel contesto corrente 
+        return expr.evaluate(this); //Evaluiate the expression in the current context  
     }
 }
